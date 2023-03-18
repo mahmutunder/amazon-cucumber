5a612da940e6ba145448efce5357dd82fbbd795c
mahmutunder <mahmutunder@gmail.com>
2023-03-17 21:13:57 -0600

    build frame worked

--- src/test/java/com/amazon/step_definitions/Hooks.java
+++ src/test/java/com/amazon/step_definitions/Hooks.java
@@ -21,6 +21,6 @@ public class Hooks {
         }
 
 
-        Driver.close();
+       Driver.closeDriver();
     }
 }
--- src/test/java/com/amazon/utilities/Driver.java
+++ src/test/java/com/amazon/utilities/Driver.java
@@ -1,60 +1,129 @@
 package com.amazon.utilities;
 
+import com.amazon.utilities.ConfigurationReader;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
+import java.net.URL;
 import java.time.Duration;
 
 public class Driver {
-
-    private static InheritableThreadLocal<WebDriver> driverPool = new InheritableThreadLocal<>();
+    static String browser;
 
     private Driver() {
     }
 
+    private static InheritableThreadLocal<WebDriver> driverPool = new InheritableThreadLocal<>();
 
     public static WebDriver getDriver() {
 
-        String browserType = ConfigurationReader.getProperty("browser");
 
         if (driverPool.get() == null) {
 
-            switch (browserType) {
+
+            if (System.getProperty("BROWSER") == null) {
+                browser = ConfigurationReader.getProperty("browser");
+            } else {
+                browser = System.getProperty("BROWSER");
+            }
+            System.out.println("Browser: " + browser);
+
+
+            switch (browser) {
+                case "remote-chrome":
+                    try {
+                        // assign your grid server address
+                        String gridAddress = "54.89.91.208";
+                        URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
+                        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+                        desiredCapabilities.setBrowserName("chrome");
+                        driverPool.set(new RemoteWebDriver(url, desiredCapabilities)); ;
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case "remote-firefox":
+                    try {
+                        // assign your grid server address
+                        String gridAddress = "54.89.91.208";
+                        URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
+                        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+                        desiredCapabilities.setBrowserName("firefox");
+                        driverPool.set(new RemoteWebDriver(url, desiredCapabilities));
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    break;
                 case "chrome":
-                    driverPool.set(new ChromeDriver());
+                    //WebDriverManager.chromedriver().setup();//if we use selenium 3. we need to use Webdriver manager
+                    ChromeOptions options=new ChromeOptions();//i added this line from Adams video for dont have 403 error code
+
+                    options.addArguments("--remote-allow-origins=*");//this line also from adam
+                    driverPool.set(new ChromeDriver(options));//i add inside chrome driver (options) from adams video
                     break;
-                case "safari":
-                    driverPool.set(new SafariDriver());
+                case "chrome-headless":
+                   // WebDriverManager.chromedriver().setup();
+
+                    driverPool.set( new ChromeDriver(new ChromeOptions().setHeadless(true)));
+
                     break;
-                case "edge":
-                    driverPool.set(new EdgeDriver());
-                    break;
+
                 case "firefox":
+                 //   WebDriverManager.firefoxdriver().setup();
                     driverPool.set(new FirefoxDriver());
                     break;
+                case "firefox-headless":
+                    WebDriverManager.firefoxdriver().setup();
+                    driverPool.set(new FirefoxDriver(new FirefoxOptions().setHeadless(true)));
+                    break;
+
+                case "ie":
+                    if (System.getProperty("os.name").toLowerCase().contains("mac")) {
+                        throw new WebDriverException("Your operating system does not support the requested browser");
+                    }
+                    WebDriverManager.iedriver().setup();
+                    driverPool.set(new InternetExplorerDriver());
+                    break;
+
+                case "edge":
+                    if (System.getProperty("os.name").toLowerCase().contains("mac")) {
+                        throw new WebDriverException("Your operating system does not support the requested browser");
+                    }
+                    WebDriverManager.edgedriver().setup();
+                    driverPool.set(new EdgeDriver());
+                    break;
+
+                case "safari":
+                    if (System.getProperty("os.name").toLowerCase().contains("windows")) {
+                        throw new WebDriverException("Your operating system does not support the requested browser");
+                    }
+                    WebDriverManager.getInstance(SafariDriver.class).setup();
+                    driverPool.set(new SafariDriver());
+                    break;
             }
-
-            driverPool.get().manage().window().maximize();
-            driverPool.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
-
+            driverPool.get().manage().window().setSize(new Dimension(1980,1020));
+            driverPool.get().manage().deleteAllCookies();
+            driverPool.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); // for selenium 3, there is no Duration method, so comment out
         }
-
 
         return driverPool.get();
     }
 
-
-
-    public static void close(){
-
-        if(driverPool.get() !=null){
+    public static void closeDriver() {
+        if (driverPool.get() != null) {
             driverPool.get().quit();
             driverPool.remove();
         }
-
     }
-
 }
